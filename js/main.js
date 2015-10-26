71e96865648037a46543005246018e68885c6293
james porter <jamesp21@uw.edu>
2015-10-26 10:21:38 -0700

    not finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 /* your code goes here! */
 
+$('#change-content').click(function(){
+    var modifySelector = $('#selector').val();
+    var content = $('#newContent').val();
+
+    $('' + modifySelector).text('' + content);
+})
+
+$('#add-at-end').click(function(){
+    var modifySelector = $('#selector').val();
+    var content = $('#newContent').val();
+    $('' + modifySelector).append('' + content);
+})
+
+$('#add-at-start').click(function(){
+    var modifySelector = $('#selector').val();
+    var content = $('#newContent').val();
+    $('' + modifySelector).prepend('' + content);
+})
+
+$('#insert-before').click(function(){
+    var modifySelector = $('#selector').val();
+    var content = $('#newContent').val();
+    $('' + modifySelector).before('' + content);
+})
+
+$('#move-after').click(function(){
+    var modifySelector = $('#selector').val();
+    var content = $('#newContent').val();
+    $('' + modifySelector).after('' + content);
+})
 /* Use clever selections and event handlers to assign the described interactivity */
 
